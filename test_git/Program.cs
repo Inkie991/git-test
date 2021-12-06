ebd606d33e454c8dccfa31d7d9f46b8a94d368e1
Inkie991 <inkie991@gmail.com>
2021-12-06 18:13:45 +0200

    added console writeline

--- test_git/Program.cs
+++ test_git/Program.cs
@@ -7,6 +7,7 @@ namespace test_git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("TEEEEEEST!!!!");
         }
     }
 }
