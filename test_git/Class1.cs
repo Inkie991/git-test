834e43dc6104cc60637d6234b2587392f69be03c
Inkie991 <inkie991@gmail.com>
2021-12-06 16:56:50 +0200

    Version 1

---
+++ test_git/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test_git
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("One more line!");
+        }
+    }
+}
